a9c838023452de65a2c55513cad9f76b5c51c5a5
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-05-11 19:54:58 +0100

    Tamper button activated when reviwing Block already in the Chain. Tuned the enability to click prev/next and submit button

--- Blockchain_Simulator_App/MainWindow.xaml.cs
+++ Blockchain_Simulator_App/MainWindow.xaml.cs
@@ -17,35 +17,11 @@ namespace Blockchain_Simulator_App
             InitializeComponent();
         }
 
-        private void PrevButton_Click(object sender, RoutedEventArgs e)
-        {
-            nextButton.IsEnabled = true;
-            if (!(_simulatorIndex == 0))
-            { _simulatorIndex--; }
-
-            indexText.Text = _simulatorIndex.ToString();
-            timeStampText.Text = _blockchain.Chain[_simulatorIndex].TimeStamp.ToString();
-            dataText.Text = _blockchain.Chain[_simulatorIndex].Data;
-            difficultyText.Text = _blockchain.Chain[_simulatorIndex].Difficulty.ToString();
-            nonceText.Text = _blockchain.Chain[_simulatorIndex].Nonce.ToString();
-            prevHashText.Text = _blockchain.Chain[_simulatorIndex].PrevHash.ToString();
-            hashText.Text = _blockchain.Chain[_simulatorIndex].Hash.ToString();
-        }
-
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
             if (difficultyText.Text == "")
             { difficultyText.Text = "0"; }
-
-            if (_simulatorIndex >= _blockchain.Chain.Count - 1) // Is it a new Block?
             { _blockchain.AddBlock(_block = new Block(int.Parse(difficultyText.Text), dataText.Text)); }
-            else
-            {
-                _block = _blockchain.Chain[int.Parse(indexText.Text)];
-                _block.Data = dataText.Text; // If existing Block, assign new Data
-                _block.Hash = _blockchain.Chain[int.Parse(indexText.Text)].CalculateHashKey_Sha256(); // Recalculate Hash Key with new Data
-                _blockchain.IsChainValid();
-            }
             // Output Block's values
             indexText.Text = _block.Index.ToString();
             _simulatorIndex = _block.Index;
@@ -57,24 +33,61 @@ namespace Blockchain_Simulator_App
             if (Blockchain.IsValidBlock)
             {
                 historyText.Text += "\n" + _block.ToString();
-                prevButton.IsEnabled = true;
-            }
-            else
-            {
-                borderColour.Background = Brushes.IndianRed; // Not Valid colour
-                submitButton.IsEnabled = false; // Locks the form to future entries
-                string message = "Hash Key does not match next Block Previous Hash Key: ";
-                string caption = _blockchain.Chain[int.Parse(indexText.Text) + 1].PrevHash;
-                MessageBox.Show(message + caption, "Invalid Block!");
-                newChainButton.IsEnabled = true;
+                if (_blockchain.Chain.Count > 1)
+                { prevButton.IsEnabled = true; }
             }
         }
 
-        private void NextButton_Click(object sender, RoutedEventArgs e)
+        private void TamperButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!(_simulatorIndex == _blockchain.Chain.Count - 1))
-            { _simulatorIndex++; }
+            _block = _blockchain.Chain[int.Parse(indexText.Text)];
 
+            if (_block.Data != dataText.Text)
+            {
+                _block.Data = dataText.Text;
+                string recordedHash = hashText.Text;
+                _blockchain.Chain[int.Parse(indexText.Text)].MiningBlock(_blockchain.Chain[int.Parse(indexText.Text)].Difficulty);
+               // _block.Hash = _blockchain.Chain[int.Parse(indexText.Text)].CalculateHashKey_Sha256(); // Recalculate Hash Key with new Data
+                _blockchain.IsChainValid();
+
+                // Output Block's values
+                indexText.Text = _block.Index.ToString();
+                _simulatorIndex = _block.Index;
+                timeStampText.Text = _block.TimeStamp.ToString();
+                nonceText.Text = _block.Nonce.ToString();
+                prevHashText.Text = _block.PrevHash;
+                hashText.Text = _block.Hash;
+                if (!Blockchain.IsValidBlock || _block.Hash != recordedHash)
+                {
+                    borderColour.Background = Brushes.IndianRed; // Not Valid colour
+                                                                 // Locks the form to future entries
+                    submitButton.IsEnabled = false;
+                    tamperButton.IsEnabled = false;
+                    prevButton.IsEnabled = false;
+                    nextButton.IsEnabled = false;
+                    string message = "Hash Key has been tampered and does not match the Hash Key previously recorded: ";
+                    MessageBox.Show(message + recordedHash, "Invalid Block!");
+                    newChainButton.IsEnabled = true;
+                }
+            }
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            difficultyText.IsEnabled = false;
+            submitButton.IsEnabled = false;
+            tamperButton.IsEnabled = true;
+            if (!(_simulatorIndex == 0))
+            {
+                _simulatorIndex--;
+                dataText.IsEnabled = true;
+                nextButton.IsEnabled = true;
+            }
+            if (_simulatorIndex == 0)
+            {
+                dataText.IsEnabled = false;
+                prevButton.IsEnabled = false;
+            }
             indexText.Text = _simulatorIndex.ToString();
             timeStampText.Text = _blockchain.Chain[_simulatorIndex].TimeStamp.ToString();
             dataText.Text = _blockchain.Chain[_simulatorIndex].Data;
@@ -84,11 +97,42 @@ namespace Blockchain_Simulator_App
             hashText.Text = _blockchain.Chain[_simulatorIndex].Hash.ToString();
         }
 
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_simulatorIndex < _blockchain.Chain.Count - 1)
+            {
+                _simulatorIndex++;
+                dataText.IsEnabled = true;
+                prevButton.IsEnabled = true;
+
+                indexText.Text = _simulatorIndex.ToString();
+                timeStampText.Text = _blockchain.Chain[_simulatorIndex].TimeStamp.ToString();
+                dataText.Text = _blockchain.Chain[_simulatorIndex].Data;
+                difficultyText.Text = _blockchain.Chain[_simulatorIndex].Difficulty.ToString();
+                nonceText.Text = _blockchain.Chain[_simulatorIndex].Nonce.ToString();
+                prevHashText.Text = _blockchain.Chain[_simulatorIndex].PrevHash.ToString();
+                hashText.Text = _blockchain.Chain[_simulatorIndex].Hash.ToString();
+            }
+            else
+            {
+                indexText.Text = System.String.Empty;
+                timeStampText.Text = System.String.Empty;
+                dataText.Text = System.String.Empty;
+                difficultyText.Text = System.String.Empty;
+                nonceText.Text = System.String.Empty;
+                prevHashText.Text = System.String.Empty;
+                hashText.Text = System.String.Empty;
+                difficultyText.IsEnabled = true;
+                submitButton.IsEnabled = true;
+                tamperButton.IsEnabled = false;
+                nextButton.IsEnabled = false;
+            }
+        }
+
         private void NewChainButton_Click(object sender, RoutedEventArgs e)
         {
             Blockchain _blockchain = new Blockchain();
             _simulatorIndex = 0;
-
             indexText.Text = System.String.Empty;
             timeStampText.Text = System.String.Empty;
             dataText.Text = System.String.Empty;
@@ -98,12 +142,11 @@ namespace Blockchain_Simulator_App
             hashText.Text = System.String.Empty;
             historyText.Text = System.String.Empty;
             borderColour.Background = Brushes.White; // Default colour
+            prevButton.IsEnabled = false;
             submitButton.IsEnabled = true;
-
-            // foreach(Text ctl in TemplateA.Children)
-            // {
-            //    ctl.Text = System.String.Empty;
-            // }
+            tamperButton.IsEnabled = false;
+            nextButton.IsEnabled = false;
+            newChainButton.IsEnabled = false;
         }
 
     }
